6046413f3b51e85d3f39b8308445f1736ee3f5f2
Fernanda López <fer.daye.2007@gmail.com>
2024-02-01 17:37:58 -0600

    mas cambios 😐

--- backend/controllers/tareasControllers.js
+++ backend/controllers/tareasControllers.js
@@ -1,25 +1,30 @@
-const asyncHandler = require( 'express-async-handler')
+const { json } = require("express");
+const asyncHandler = require("express-async-handler");
 
-const getTareas = asyncHandler( async(req, res) =>{
-    res.status(200).json({ mensaje: ' Tareas'})
-})
+const getTareas = asyncHandler(async (req, res) => {
+  res.status(200).json({ mensaje: " Tareas" });
+});
 
-const createTareas = asyncHandler( async(req, res) =>{
-    if(!req.body.descripcion){
-        res.status(400)
-        throw new Error (' ingresa una descripcion ')
-    }
-})
-const updateTareas = asyncHandler (async(req, res) =>{
-    res.status(200).json({ mensaje: `Modificar la tarea con id ${req.params.id}`})
-})
-const deleteTareas =asyncHandler( async(req, res) =>{
-    res.status(200).json({ mensaje: `id: ${req.params.id}`})
-})
+const createTareas = asyncHandler(async (req, res) => {
+  if (!req.body.descripcion) {
+    res.status(400);
+    throw new Error("ingresa una descripcion ");
+  }
+  res.status(201)
+  console.log(req.body);
+});
+const updateTareas = asyncHandler(async (req, res) => {
+  res
+    .status(200)
+    .json({ mensaje: `Modificar la tarea con id ${req.params.id}` });
+});
+const deleteTareas = asyncHandler(async (req, res) => {
+  res.status(200).json({ mensaje: `id: ${req.params.id}` });
+});
 
 module.exports = {
-    getTareas,
-    createTareas,
-    updateTareas,
-    deleteTareas
-}
+  getTareas,
+  createTareas,
+  updateTareas,
+  deleteTareas,
+};
