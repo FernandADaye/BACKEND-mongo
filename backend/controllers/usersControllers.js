54680da250faa977808c1aadd544b93e0f6e1a27
Fernanda López <fer.daye.2007@gmail.com>
2024-02-01 20:42:43 -0600

    controllers de Usuarios

--- backend/controllers/usersControllers.js
+++ backend/controllers/usersControllers.js
@@ -1,3 +1,15 @@
 const crearUser = (req, res) =>{
-    res.status(201)
+    res.status(201).json({message:'Crear usuario '})
+}
+const loginUser = (req, res) =>{
+    res.status(201).json({message:'login usuario '})
+}
+const datoUser = (req, res) =>{
+    res.status(201).json({message:'datos del usuario '})
+}
+
+module.exports={
+crearUser,
+loginUser,
+datoUser
 }
