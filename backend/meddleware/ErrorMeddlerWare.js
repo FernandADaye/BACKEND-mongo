66ba46b003f06cabc557ac6d0603629295686b8a
Fernanda López <fer.daye.2007@gmail.com>
2024-02-02 13:01:10 -0600

    ajuste en mi manejo de errores

--- backend/meddleware/ErrorMeddlerWare.js
+++ backend/meddleware/ErrorMeddlerWare.js
@@ -11,8 +11,23 @@
 // 
 
 
+// const errorHandler = (err, req, res, next) => {
+    // const statusCode = res.statusCode ? res.statusCode : 500;
+    // res.status(statusCode).json({
+        // message: err.message,
+        // stack: process.env.NODE_ENV === 'production' ? null : err.stack 
+    // });
+// };
+// 
+// module.exports = {
+    // errorHandler
+// };
+// 
 const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : 500;
+    
+    console.error('Error en el middleware de manejo de errores:', err.message);
+
     res.status(statusCode).json({
         message: err.message,
         stack: process.env.NODE_ENV === 'production' ? null : err.stack 
