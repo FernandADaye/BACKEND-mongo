daa181b2a6a15b16a499ad9731012bf6c5b2b906
Fernanda López <fer.daye.2007@gmail.com>
2024-02-02 09:46:57 -0600

    ajuste de texto

--- backend/routes/tareasRoutes.js
+++ backend/routes/tareasRoutes.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const router = express.Router()
-const {getTareas} = require('../controllers/tareasControllers')
-const {createTareas} = require('../controllers/tareasControllers')
-const {updateTareas} = require('../controllers/tareasControllers')
-const {deleteTareas} = require('../controllers/tareasControllers')
+const {getTareas,createTareas,updateTareas,deleteTareas} = require('../controllers/tareasControllers')
 
 router.get('/', getTareas)
-    
 
 router.post('/', createTareas)
 
