52d886c1fa773d818538f3333cd1ab6be86055f2
Fernanda López <fer.daye.2007@gmail.com>
2024-02-01 21:31:34 -0600

    por el momento nada está correcto, asi que ni lo veas

--- backend/controllers/usersControllers.js
+++ backend/controllers/usersControllers.js
@@ -9,6 +9,14 @@ const crearUser = asyncHandler(  async(req, res) =>{
         res.status(400)
         throw new Error('faltan datos')
     }
+
+    //verificar que ese usuario no exista a travez de su email 
+    const userExiste = await User.findOne({email})
+    if (userExiste) {
+        res.status(400)
+        throw new Error('Ese usuario ya existe ')
+    }
+
     res.status(201).json({message:'Crear usuario '})
 })
 const loginUser = asyncHandler( async(req, res) =>{
@@ -17,6 +25,28 @@ const loginUser = asyncHandler( async(req, res) =>{
 const datoUser = asyncHandler( async(req, res) =>{
     res.status(201).json({message:'datos del usuario '})
 })
+
+// HASH PASSWORD 
+const salt = await bcrypt.genSalt(10)
+Password = await bcrypt.hash(password,salt)
+
+//crear el usuario 
+const user = await User.create({
+    name, 
+    email, 
+    password: hashedPassword 
+})
+
+if (user) {
+    res.status(201).json({
+        _id: user.id,
+        name: user.name,
+        email: user.email
+    })
+} else {
+    res.status(400)
+    throw new Error('No se pudieron guardar los datos')
+}
 
 module.exports={
 crearUser,
