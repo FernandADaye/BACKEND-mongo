98496a6eeacde6d2ece5178aed45d693eb9ebdea
Fernanda López <fer.daye.2007@gmail.com>
2024-02-01 21:10:50 -0600

    crear usuario

--- backend/controllers/usersControllers.js
+++ backend/controllers/usersControllers.js
@@ -1,12 +1,22 @@
-const crearUser = (req, res) =>{
+const   jwt = require ('jsonwebtoken')
+const bcrypt =require('bcryptjs')
+const asyncHandler =    require ('express-async-handler')
+const User= require('../models/usersModel')
+
+const crearUser = asyncHandler(  async(req, res) =>{
+    const {name, email, password } =  req.body
+    if (!name || !email || !password ) {
+        res.status(400)
+        throw new Error('faltan datos')
+    }
     res.status(201).json({message:'Crear usuario '})
-}
-const loginUser = (req, res) =>{
+})
+const loginUser = asyncHandler( async(req, res) =>{
     res.status(201).json({message:'login usuario '})
-}
-const datoUser = (req, res) =>{
+})
+const datoUser = asyncHandler( async(req, res) =>{
     res.status(201).json({message:'datos del usuario '})
-}
+})
 
 module.exports={
 crearUser,
