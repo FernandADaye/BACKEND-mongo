d284d8956c6c71c86d3e9bbe2d0476781afeadbf
Fernanda López <fer.daye.2007@gmail.com>
2024-02-19 10:22:41 -0600

    aun con un error

--- backend/service.js
+++ backend/service.js
@@ -4,16 +4,22 @@ const dotenv = require('dotenv').config()
 const port = process.env.PORT || 3001
 const conectdb= require('../backend/config/db')
 const colors = require('colors')
-const {errorHandler} = require ('../backend/meddleware/ErrorMeddlerWare')
-
-conectdb();
+const errorHandler = require ('../backend/meddleware/ErrorMeddlerWare.js')
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
+conectdb();
+
+
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
+
 app.use('/tarea', require('./routes/tareasRoutes'))
 app.use('/tarea/users', require('./routes/tareasRoutes'))
+
 app.use(errorHandler);
 
-app.listen(port, ()=>console.log(`por fin jalo en el puerto ${port}`))
+
+app.listen(port, () => console.log(`por fin jalo en el puerto ${port}`))
 
