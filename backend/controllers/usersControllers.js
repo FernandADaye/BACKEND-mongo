1f32de2823f588ab19cdf85eec8282fff46998c6
Fernanda López <fer.daye.2007@gmail.com>
2024-02-19 17:18:11 -0600

    comenzar con users y hash

--- backend/controllers/usersControllers.js
+++ backend/controllers/usersControllers.js
@@ -1,42 +1,42 @@
-// const   jwt = require ('jsonwebtoken')
-// const bcrypt =require('bcryptjs')
-// const asyncHandler =    require ('express-async-handler')
-// const User= require('../models/usersModel')
-// 
-// const crearUser = asyncHandler(  async(req, res) =>{
-    // const {name, email, password } =  req.body
-    // if (!name || !email || !password ) {
-        // res.status(400)
-        // throw new Error('faltan datos')
-    // }
-// 
+    const   jwt = require ('jsonwebtoken')
+    const bcrypt =require('bcryptjs')
+    const asyncHandler =    require ('express-async-handler')
+    const User= require('../models/usersModel')
+
+const crearUser = asyncHandler(  async(req, res) =>{
+    const {name, email, password } =  req.body
+    if (!name || !email || !password ) {
+        res.status(400)
+        throw new Error('faltan datos')
+    }
+
     // verificar que ese usuario no exista a travez de su email 
-    // const userExiste = await User.findOne({email})
-    // if (userExiste) {
-        // res.status(400)
-        // throw new Error('Ese usuario ya existe ')
-    // }
-// 
-    // res.status(201).json({message:'Crear usuario '})
-// })
-// const loginUser = asyncHandler( async(req, res) =>{
-    // res.status(201).json({message:'login usuario '})
-// })
-// const datoUser = asyncHandler( async(req, res) =>{
-    // res.status(201).json({message:'datos del usuario '})
-// })
-// 
-// HASH PASSWORD 
+    const userExiste = await User.findOne({email})
+    if (userExiste) {
+        res.status(400)
+        throw new Error('Ese usuario ya existe ')
+    }
+
+    res.status(201).json({message:'Crear usuario '})
+})
+const loginUser = asyncHandler( async(req, res) =>{
+    res.status(201).json({message:'login usuario '})
+})
+const datoUser = asyncHandler( async(req, res) =>{
+    res.status(201).json({message:'datos del usuario '})
+})
+
+//HASH PASSWORD 
 // const salt = await bcrypt.genSalt(10)
 // Password = await bcrypt.hash(password,salt)
 // 
-// crear el usuario 
+//crear el usuario 
 // const user = await User.create({
     // name, 
     // email, 
     // password: hashedPassword 
 // })
-// 
+
 // if (user) {
     // res.status(201).json({
         // _id: user.id,
@@ -47,25 +47,22 @@
     // res.status(400)
     // throw new Error('No se pudieron guardar los datos')
 // }
-// 
-// module.exports={
-// crearUser,
-// loginUser,
-// datoUser
-// }
 
-
-const crearUser = ( res, req)=> {
-    res.status(200).json({message: 'crear usuario' })
-}
-const loginUser = ( res, req)=> {
-    res.status(200).json({message: 'loginUser' })
-}
-const datoUser = ( res, req)=> {
-    res.status(200).json({message: 'dato user ' })
-}
 module.exports={
-    crearUser,
-    loginUser,
-    datoUser
-    }
+crearUser,
+loginUser,
+datoUser
+}
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+
+
