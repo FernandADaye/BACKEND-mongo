872eec71e9e51120e9ee98264fe52b4cf7464c5e
Fernanda López <fer.daye.2007@gmail.com>
2024-02-20 20:12:32 -0600

    loginUser

--- backend/controllers/usersControllers.js
+++ backend/controllers/usersControllers.js
@@ -45,18 +45,33 @@ if (user) {
 
 
 
-
-
-
-
-
-
 const loginUser = asyncHandler( async(req, res) =>{
+
+    const { email, password} = req.body
+
+    const user = await User.findOne({email})
+
+if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+        _id: user.id,
+        name: user.name,
+        email:user.email,
+    })
+}else{
+    res.status(400)
+    throw new Erorr ('no se puedo hacer eso ')
+}
+
+
     res.status(201).json({message:'login usuario '})
 })
 const datoUser = asyncHandler( async(req, res) =>{
     res.status(201).json({message:'datos del usuario '})
 })
+
+
+
+
 
 //HASH PASSWORD 
 // const salt = await bcrypt.genSalt(10)
--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const{crearUser,loginUser,datoUser}=require('../controllers/usersControllers')
 // const { protect } = require('../middleware/authMiddleware')
-router.post ('/', crearUser)
+router.post ('/crear', crearUser)
 router.post ('/login', loginUser)
 router.get ('/datos', datoUser)
 
